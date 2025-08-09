f256b84b5717d0f24d808a474f382824e5809282
thesmartcat76 <i.y.hochreiter@gmail.com>
2025-08-09 08:23:54 -0400

    upde

---
+++ script.js
@@ -0,0 +1,10 @@
+function qu(){
+    let qus = ["1.\n would you call yourself\n a. kind\n b passonite\n or c. mean", "", ""];
+    let num = 0;
+    let idx = num - 1;
+    let qualefing = ["kind", "girly", "addicted"];
+    let answers = [];
+    let answer = " ";
+    alert("Let's find out if you can be friends with me!")
+    alert(qus[idx])
+}
